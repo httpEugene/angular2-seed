cde7fd13c09e3d8cf1604d782ad4f0fdc89bda74
Yevhenii Kyselov <Yevhenii_Kyselov@epam.com>
2016-09-17 23:30:16 +0300

    Courses and authors components added

---
+++ app/courses/courses.component.ts
@@ -0,0 +1,24 @@
+import {Component} from 'angular2/core';
+import {CoursesService} from './courses.service';
+
+@Component({
+    selector: 'courses',
+    template: `
+        <h2>Courses</h2>
+        {{ title }}
+        <ul>
+            <li *ngFor="#course of courses">
+                {{ course }}
+            </li>
+        </ul>
+        `,
+    providers: [CoursesService]
+})
+export class CoursesComponent{
+    title = 'Title of courses';
+    courses;
+
+    constructor(coursesService: CoursesService) {
+        this.courses = coursesService.getCourses();
+    }
+}
